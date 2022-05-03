6ae637ee4d2f40d48d08aa7352c62cb0a93425e8
didwjddyd <didwjddyd13@naver.com>
2022-05-03 16:18:01 +0900

    modify main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-#  print menu button with blue background
-blue_background()
+print_main_title()
+{
+	echo -e "\n"
+	echo '   ____   ___  ____ ___ _       _ '
+	echo '  / ___| / _ \/ ___|_ _| |     / |'
+	echo '  \___ \| | | \___ \| || |     | |'
+	echo '   ___) | | | |___) | || |___  | |'
+	echo '  |____/ \___/|____/___|_____| |_|'
+	echo '      _  _____  _    __  ____  __ '
+	echo '     / \|_   _|/ \   \ \/ /\ \/ / '
+	echo '    / _ \ | | / _ \   \  /  \  /  '
+	echo '   / ___ \| |/ ___ \  /  \  /  \  '
+	echo '  /_/   \_\_/_/   \_\/_/\_\/_/\_\ '
+	echo -e "\n\c"
+	echo '                           2019203027 양정용'
+	echo -e "\n\c"
+}
+
+#  print menu button with blue or red background
+blue_background()  #  red code = 41
 {
 	text="$1"
 	maxlen=$2
@@ -34,7 +52,7 @@ blue_background()
 }
 
 #  print menu button per line
-main_menu_line()
+menu_line()
 {
 	text1="$1"
 	text2="$2"
@@ -45,26 +63,74 @@ main_menu_line()
 	echo
 }
 
+direction="A"  #  to return arrow direction from input_arrow()
+
+input_arrow()
+{
+	read -s -n 1 input
+	if [ $input = "" ]
+	then
+		read -s -n 1 input
+
+		if [ $input = "[" ]
+		then
+			read -s -n 1 input
+
+			if [ $input = "A" ]  #  UP
+			then
+				direction="A"
+			elif [ $input = "B" ]  #  DOWN
+			then
+				direction="B"
+			elif [ $input = "C" ]  #  RIGHT
+			then
+				direction="C"
+			elif [ $input = "D" ]  #  LEFT
+			then
+				direction="D"
+			fi
+		fi
+	fi
+}
+
+highlight=0  #  number of button which is highlighted
+
+clear
+print_main_title
+menu_line JOIN "SIGN IN"
+echo -e "\n"
+menu_line EXIT "SIGN OUT"
+echo -e "\n"
+
 while true
 do
+	input_arrow
 	clear
-	
-	echo -e "\n"
-	echo '   ____   ___  ____ ___ _       _ '
-	echo '  / ___| / _ \/ ___|_ _| |     / |'
-	echo '  \___ \| | | \___ \| || |     | |'
-	echo '   ___) | | | |___) | || |___  | |'
-	echo '  |____/ \___/|____/___|_____| |_|'
-	echo '      _  _____  _    __  ____  __ '
-	echo '     / \|_   _|/ \   \ \/ /\ \/ / '
-	echo '    / _ \ | | / _ \   \  /  \  /  '
-	echo '   / ___ \| |/ ___ \  /  \  /  \  '
-	echo '  /_/   \_\_/_/   \_\/_/\_\/_/\_\ '
-	echo -e "\n\c"
-	echo '                           2019203027 양정용'
-	echo -e "\n\c"
-	main_menu_line JOIN "SIGN IN"
-	echo -e "\n"
-	main_menu_line EXIT "SIGN OUT"
-	echo -e "\n"
+	print_main_title
+	case highlight in
+		0)
+			if [ $direction = "B" ]
+			then
+			elif [ $direction = "C" ]
+			then
+			fi;;
+		1)
+			if [ $direction = "B" ]
+			then
+			elif [ $direction = "D" ]
+			then
+			fi;;
+		2)
+			if [ $direction = "A" ]
+			then
+			elif [$direction = "C" ]
+			then
+			fi;;
+		3)
+			if [ $direction = "A" ]
+			then
+			elif [ $direction = "D" ]
+			then
+			fi;;
+	esac
 done
